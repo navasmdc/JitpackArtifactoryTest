ed9b32fc72ab02afd9c82eec659ed86eb55be8a8
Ivan Navas <i.navas.mdc@gmail.com>
2019-03-27 17:04:35 +0100

    Added library dependency to app

--- app/src/main/java/com/github/artifactorytest/MainActivity.kt
+++ app/src/main/java/com/github/artifactorytest/MainActivity.kt
@@ -2,11 +2,13 @@ package com.github.artifactorytest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.mathutils.MathUtils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MathUtils.sum(1,2)
     }
 }
